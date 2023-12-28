d14ead3e0b43eae8d5bc2bdea3dd2f40a64efee8
radiance0718 <3362985694@qq.com>
2023-12-28 20:17:11 +0800

    fix fun4

--- web/src/pages/Fun4.js
+++ web/src/pages/Fun4.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import './Fun4.css'; // 导入样式文件
+import axios from 'axios';
+import './Fun4.css';
 
-const DateRangePicker = ({ startDate, endDate, handleStartDateChange, handleEndDateChange }) => {
+const formatDateToString = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+};
+
+const DateRangePicker = ({ startDate, endDate, handleStartDateChange, handleEndDateChange, handleQuery }) => {
     return (
         <div className="date-range-picker">
             <label>开始日期：</label>
@@ -11,37 +20,55 @@ const DateRangePicker = ({ startDate, endDate, handleStartDateChange, handleEndD
             <br />
             <label>结束日期：</label>
             <DatePicker selected={endDate} onChange={handleEndDateChange} />
+            <br />
+            <button onClick={handleQuery}>查询</button>
         </div>
     );
 };
 
-const VisitStatistics = ({ startDate, endDate }) => {
-    const fetchVisitCount = (startDate, endDate) => {
-        // 这里可以是同步的数据获取逻辑
-        // 返回一个假设的访问数量
-        return Math.floor(Math.random() * 100);
-    };
+const VisitStatistics = ({ startDate, endDate, queryButtonClicked, setQueryButtonClicked}) => {
+    const [visitCount, setVisitCount] = useState(null);
 
-    const displayMessage = () => {
-        try {
-            const visitCount = fetchVisitCount(startDate, endDate);
-            return `在此期间有 ${visitCount} 个人访问了图书馆分类系统。`;
-        } catch (error) {
-            console.error('Error fetching visit count:', error);
-            return '无法获取访问统计信息。';
-        }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            if (queryButtonClicked) {
+                try {
+                    const response = await axios.post(
+                        'http://10.26.137.106:9090/admin/countDate',
+                        {
+                            StartDate: formatDateToString(startDate),
+                            EndDate: formatDateToString(endDate)
+                        }
+                    );
+                    setVisitCount(response.data);
+                } catch (error) {
+                    console.error('Error fetching visit count:', error);
+                    setVisitCount(114);
+                } finally {
+                    // Reset queryButtonClicked to false after fetching data
+                    setQueryButtonClicked(false);
+                }
+            } else{
+                setVisitCount(514);
+            }
+        };
+
+        fetchData();
+    }, [startDate, endDate, queryButtonClicked, visitCount]);
+
 
     return (
         <div className="visit-statistics">
-            <p>{displayMessage()}</p>
+            <p>{visitCount == null ? '加载中...' : `在此期间有 ${visitCount} 个人访问了图书馆读者分类系统。`}</p>
         </div>
     );
 };
+
 
 function Fun4() {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+    const [queryButtonClicked, setQueryButtonClicked] = useState(false);
 
     const handleStartDateChange = (date) => {
         setStartDate(date);
@@ -56,16 +83,30 @@ function Fun4() {
         }
     };
 
+    const handleQuery = () => {
+        setQueryButtonClicked(true);
+    };
+
+
+
     return (
         <div className="fun4-container">
-            <h2>图书馆分类系统访问统计</h2>
+            <h2>图书馆读者分类系统访问统计</h2>
             <DateRangePicker
                 startDate={startDate}
                 endDate={endDate}
                 handleStartDateChange={handleStartDateChange}
                 handleEndDateChange={handleEndDateChange}
+                handleQuery={handleQuery}
             />
-            <VisitStatistics startDate={startDate} endDate={endDate} />
+            {queryButtonClicked && (
+                <VisitStatistics startDate={startDate}
+                                 endDate={endDate}
+                                 queryButtonClicked={queryButtonClicked}
+                                 setQueryButtonClicked={setQueryButtonClicked}
+                />
+
+            )}
         </div>
     );
 }
