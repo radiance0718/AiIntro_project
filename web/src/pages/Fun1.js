fa9b7ca5a6ce3244762d0884f337d38b68ebf994
Rongfei Zheng <rongfeizheng@MacBook-Pro.local>
2023-12-28 19:23:11 +0800

    更新fun1

--- web/src/pages/Fun1.js
+++ web/src/pages/Fun1.js
@@ -8,7 +8,16 @@ function Fun1() {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [queryResult, setQueryResult] = useState('');
 
-  const categories = ['1', '2', '3','4','5','6']; // 示例分类
+  // 使用对象数组来定义分类和它们的汉字表示
+  const categories = [
+    { label: '阅览者', value: '1' },
+    { label: '钻研者', value: '2' },
+    { label: '思辨者', value: '3' },
+    { label: '勤学者', value: '4' },
+    { label: '学习者', value: '5' },
+    { label: '无闻者', value: '6' }
+    // ...可以根据需要添加更多分类
+  ];
 
   const handleCategoryChange = value => {
     setSelectedCategory(value);
@@ -16,18 +25,13 @@ function Fun1() {
 
   const handleQuery = async () => {
     try {
-        const response = await axios.post('http://10.26.137.106:9090/admin/countType', {
-            Type: selectedCategory
-          });
-          
-
-
+      const response = await axios.post('http://10.26.137.106:9090/admin/countType', {
+        Type: selectedCategory
+      });
       setQueryResult(response.data);
-       // 假设返回的数据在 response.data 中
-       console.log(response.data)
+      console.log(response.data);
     } catch (error) {
       console.error('查询失败:', error);
-      // 处理错误情况
     }
   };
 
@@ -37,7 +41,7 @@ function Fun1() {
         <Col>
           <Select value={selectedCategory} onChange={handleCategoryChange} style={{ width: 200 }}>
             {categories.map(category => (
-              <Option key={category} value={category}>{category}</Option>
+              <Option key={category.value} value={category.value}>{category.label}</Option>
             ))}
           </Select>
         </Col>
